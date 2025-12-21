d71043fa62bfd5f3987cf22fa96f652cce04afd3
NikitaZheleznov <nikitazh99@mail.ru>
2025-12-22 00:57:26 +0400

    hw08

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) == 0 {
+		fmt.Fprintf(os.Stderr, "no command specified\n")
+		return 1
+	}
+
+	prepareEnvironment(env)
+
+	command := exec.Command(cmd[0], cmd[1:]...)
+
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	command.Env = os.Environ()
+
+	if err := command.Run(); err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
+				return status.ExitStatus()
+			}
+		}
+		fmt.Fprintf(os.Stderr, "failed to run command: %v\n", err)
+		return 1
+	}
+
+	return 0
+}
+
+func prepareEnvironment(env Environment) {
+	for name, envValue := range env {
+		if envValue.NeedRemove {
+			os.Unsetenv(name)
+		} else {
+			os.Setenv(name, envValue.Value)
+		}
+	}
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <env-dir> <command> [args...]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	envDir := os.Args[1]
+	command := os.Args[2]
+	args := os.Args[3:]
+
+	env, err := ReadDir(envDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading environment directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	cmd := []string{command}
+	cmd = append(cmd, args...)
+
+	returnCode := RunCmd(cmd, env)
+	os.Exit(returnCode)
 }
